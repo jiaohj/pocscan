07b85f739db3fdef9741f7b84685dd422cec96a6
jiaohj <jiaohj@jingoal.com>
2016-12-02 00:29:24 +0800

    add start,suspend,resme interface

--- pocscan/work/urls.py
+++ pocscan/work/urls.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-
 urlpatterns = patterns(
     'pocscan.work.views',
     url(r'^upload/$', 'upload_plug', name='upload'),
--- pocscan/work/views.py
+++ pocscan/work/views.py
@@ -6,12 +6,67 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from .utils import jsonify
+import time
+import uuid
+import json
 
 
 @csrf_exempt
 @jsonify
 def upload_plug(request, *args, **kwargs):
-    return dict(state=True)
+    current_time=time.strftime("%Y%m%d%H%M%S")
+    random=uuid._random_getnode()
+    unique_ident=current_time+str(random)    #uuid
+    filename=str(request.FILES['pocfile'])   #poc filename
+    file_path='/tmp/'                        #file store path
+    fp=open("%s%s"%(file_path,unique_ident),'w') #{uuid}.py 
+    fp.write(filename.read())
+    fp.close()
+    PlugRecord.objects.create(uuid=unique_ident)
+
+    
+    return HttpResponse(json.dumps({"filename":filename,"uuid":unique_ident}),content_type='application/json')
 
 
+def get_poc_path(uuid): 
+    path=PlugRecord.objects.get(uuid=uuid)
+    poc_file_path=path+"/"+uuid+".py"
+    return path
 
+
+def make_task(request):
+    '''
+     purpose: run task
+     return child process object instance
+    
+    '''
+    task_id=request.POST['taskid']
+    poc_file_path = get_poc_path(uuid)
+    task_start_time= time.strftime("%Y-%m-%d %H:%m:%s")
+    child_process=subprocess.Popen(["python","{0}".format(poc_file_path)],stdout=subprocess.PIPE)
+    pid=child_process.pid
+    PocTask.objects.create(task_id=task_id,local_task_id=pid)
+    #s.wait() 
+    return child_process
+
+def suspend_task(request):
+    '''
+    return task status
+    '''	
+    task_id=request.POST['task_id']
+    result=PocTask.objects.get(task_id=task_id)
+    pid=result.local_task_id
+    process = psutil.Process(pid)
+    process.suspend()
+    task_status=process.status()
+    return task_status   
+
+
+def resume_task(pid):
+    '''
+    return task status
+    '''
+    process = psutil.Process(pid)
+    process.resume()
+    task_status=process.status()
+    return task_status
